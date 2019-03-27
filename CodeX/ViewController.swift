9cb8049ef07a5b268f5d3cc4cfa1295bb2df8218
Sachin Kumar Singh <singh.s.sachin@icloud.com>
2019-03-28 00:20:18 +0530

    Initial Commit

---
+++ CodeX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeX
+//
+//  Created by Sachin Kumar Singh on 07/01/1941 Saka.
+//  Copyright © 1941 Sachin Kumar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
